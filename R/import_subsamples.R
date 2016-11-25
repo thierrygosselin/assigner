8c98fb9a7571d65129b6dd42b7c2e34b08fa518a
Thierry Gosselin <thierrygosselin@icloud.com>
2016-11-24 23:26:21 -0500

    @importFrom work

--- R/import_subsamples.R
+++ R/import_subsamples.R
@@ -11,9 +11,10 @@
 
 #' @export
 #' @rdname import_subsamples
-#' @import dplyr
-#' @import stringi
-
+#' @importFrom dplyr bind_rows
+#' @importFrom tibble as_data_frame
+#' @importFrom stringi stri_paste stri_detect_fixed stri_replace_all_fixed
+#' @importFrom readr read_tsv
 
 #' @examples
 #' \dontrun{
@@ -25,35 +26,35 @@
 
 #' @author Thierry Gosselin \email{thierrygosselin@@icloud.com}
 
-import_subsamples <- function(dir.path, imputations){
-  if (missing (dir.path)) stop("dir.path argument missing")
-  if (missing (imputations)) imputations <- FALSE
+import_subsamples <- function(dir.path, imputations = FALSE){
+  
+  if (missing(dir.path)) stop("dir.path argument missing")
   
   sampling.method.files <- list.files(path = dir.path, pattern = "assignment", full.names = FALSE)[1]
-  sampling.method <- stri_detect_fixed(str = sampling.method.files, pattern = "ranked") # looks for ranked
+  sampling.method <- stringi::stri_detect_fixed(str = sampling.method.files, pattern = "ranked") # looks for ranked
   
   subsample.folders <- list.files(path = dir.path, pattern = "subsample_", full.names = FALSE)
   data <- list()
   for (i in subsample.folders) {
-    sub.name <- stri_replace_all_fixed(str = i, pattern = "_", replacement = ".", vectorize_all = FALSE)
-    if (sampling.method == FALSE){
-      if (imputations == TRUE){
-        filename <- stri_paste(dir.path, "/", i, "/","assignment.random.imputed.results.individuals.iterations.", sub.name, ".tsv")
+    sub.name <- stringi::stri_replace_all_fixed(str = i, pattern = "_", replacement = ".", vectorize_all = FALSE)
+    if (!sampling.method) {
+      if (imputations) {
+        filename <- stringi::stri_join(dir.path, "/", i, "/","assignment.random.imputed.results.individuals.iterations.", sub.name, ".tsv")
       } else {
-        filename <- stri_paste(dir.path, "/", i, "/","assignment.random.no.imputation.results.individuals.iterations.", sub.name, ".tsv")
+        filename <- stringi::stri_join(dir.path, "/", i, "/","assignment.random.no.imputation.results.individuals.iterations.", sub.name, ".tsv")
       }
     } else {
-      if (imputations == TRUE){
-        filename <- stri_paste(dir.path, "/", i, "/","assignment.ranked.imputed.results.individuals.iterations.", sub.name, ".tsv")
+      if (imputations) {
+        filename <- stringi::stri_join(dir.path, "/", i, "/","assignment.ranked.imputed.results.individuals.iterations.", sub.name, ".tsv")
       } else {
-        filename <- stri_paste(dir.path, "/", i, "/","assignment.ranked.no.imputation.results.individuals.iterations.", sub.name, ".tsv")
+        filename <- stringi::stri_join(dir.path, "/", i, "/","assignment.ranked.no.imputation.results.individuals.iterations.", sub.name, ".tsv")
       }
     }
-    subsample.data <- read_tsv(file = filename, col_names = TRUE) 
+    subsample.data <- readr::read_tsv(file = filename, col_names = TRUE) 
     # mutate(SUBSAMPLE = rep(i, n()))
       # filter (MISSING_DATA == 'no.imputation')
     data[[i]] <- subsample.data
   }
-  data <- as_data_frame(bind_rows(data))
+  data <- tibble::as_data_frame(dplyr::bind_rows(data))
   return(data)
 }
